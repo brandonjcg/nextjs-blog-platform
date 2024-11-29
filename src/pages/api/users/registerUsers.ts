7f274fa69a5401f84d504d09f44ac7d9650a70e9
jatin-3009 <jatinninawe02@gmail.com>
2024-11-29 18:03:43 +0530

    feat: Implement user registration API handler

---
+++ src/pages/api/users/registerUsers.ts
@@ -0,0 +1,40 @@
+import connectDB from "@/utils/db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function registerUsersHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { username, password, role } = req.body;
+
+    if (!username || !password || !role) {
+      return res
+        .status(400)
+        .json({ message: "Username, password and role are required!" });
+    }
+
+    try {
+      await connectDB();
+
+      const existingUser = await User.findOne({ username });
+
+      if (existingUser) {
+        return res.status(400).json({ message: "Username already exists" });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new User({ username, password: hashedPassword, role });
+
+      await newUser.save();
+
+      return res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+      return res.status(500).json({ message: "Error registering user", error });
+    }
+  }
+
+  return res.status(405).json({ message: "Method Not Allowed" });
+}
