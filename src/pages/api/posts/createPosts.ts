9ea65eae53cb556904c2d7b974c3775c6495a9c4
jatin-3009 <jatinninawe02@gmail.com>
2024-11-30 12:20:26 +0530

    feat: Update createPosts API to use authorName and validate user existence

--- src/pages/api/posts/createPosts.ts
+++ src/pages/api/posts/createPosts.ts
@@ -1,24 +1,36 @@
 import connectDB from "@/utils/db";
 import Post from "@/models/Post";
 import { NextApiRequest, NextApiResponse } from "next";
+import User from "@/models/User";
 
 export default async function createPostsHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    const { title, content, author } = req.body;
+    const { title, content, authorName } = req.body;
 
-    if (!title || !content || !author) {
+    if (!title || !content || !authorName) {
       return res
         .status(400)
-        .json({ message: "Title, content and author are required!" });
+        .json({ message: "Title, content and author name are required!" });
     }
 
     try {
       await connectDB();
 
-      const newPost = new Post({ title, content, author });
+      const user = await User.findOne({ username: authorName });
+
+      if (!user) {
+        return res.status(404).json({ message: "Author not found" });
+      }
+
+      const newPost = new Post({
+        title,
+        content,
+        authorId: user._id,
+        authorName: user.username,
+      });
 
       await newPost.save();
 
