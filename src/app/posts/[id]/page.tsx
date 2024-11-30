d195ef622a9658f98c2dce23cb63692db003704a
jatin-3009 <jatinninawe02@gmail.com>
2024-11-30 12:21:39 +0530

    feat: Simplify PostPage component by removing author fetching and using authorName directly

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -6,7 +6,6 @@ import { useParams } from "next/navigation";
 
 const PostPage: React.FC = () => {
   const [post, setPost] = useState<Post | null>(null);
-  const [author, setAuthor] = useState<string | null>(null);
   const { id } = useParams<{ id: string | undefined }>();
 
   console.log("id", id);
@@ -23,31 +22,13 @@ const PostPage: React.FC = () => {
     fetchPost();
   }, [id]);
 
-  useEffect(() => {
-    if (!post?.author) return;
-
-    const fetchAuthor = async () => {
-      const response = await fetch(`/api/users/getUser/${post.author}`);
-      const data = await response.json();
-
-      if (data?.user) {
-        setAuthor(data.user.username);
-      } else {
-        setAuthor("Anonymous");
-      }
-    };
-
-    fetchAuthor();
-  }, [post]);
-
   if (!post) return <div>Loading...</div>;
-  if (!author) return <div>Getting author name...</div>;
 
   return (
     <div>
       <h1>{post.title}</h1>
       <p>{post.content}</p>
-      <p>By: {author}</p>
+      <p>By: {post.authorName}</p>
     </div>
   );
 };
