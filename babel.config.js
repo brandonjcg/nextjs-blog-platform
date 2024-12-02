165359dc23d6443a6746fd66e884383e4884a199
jatin-3009 <jatinninawe02@gmail.com>
2024-12-02 11:47:27 +0530

    refactor: Simplify Babel configuration by moving test environment presets and plugins to the main configuration

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,10 @@
 module.exports = {
-    env: {
-        test: {
-            presets: [
-                'next/babel', // Ensures compatibility with Next.js
-                '@babel/preset-react', // Adds support for JSX syntax
-                '@babel/preset-typescript' // Adds TypeScript support
-            ],
-            plugins: [
-                '@babel/plugin-syntax-jsx', // Ensures that JSX syntax is properly parsed
-            ],
-        }
-    }
+    presets: [
+        'next/babel', // Ensures compatibility with Next.js
+        '@babel/preset-react', // Adds support for JSX syntax
+        '@babel/preset-typescript' // Adds TypeScript support
+    ],
+    plugins: [
+        '@babel/plugin-syntax-jsx', // Ensures that JSX syntax is properly parsed
+    ],
 };
